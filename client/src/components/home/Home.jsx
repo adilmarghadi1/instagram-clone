59c1bd541a5b62d0569b9ac30c6a6002a4d9b7a9
adilmarghadi1 <adilmarghadi2020@gmail.com>
2022-03-16 23:54:26 +0100

    Update

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -17,6 +17,9 @@ import profile1 from '../../images/profile1.jpg'
 import img17 from '../../images/img17.jpg'
 import img18 from '../../images/img18.jpg'
 import adilimg from '../../images/adil.jfif'
+import img21 from '../../images/im21.jfif'
+import img23 from '../../images/img23.jpg'
+import img24 from '../../images/img24.jpg'
 
 import {   BiDotsHorizontalRounded } from "react-icons/bi";
 
@@ -72,7 +75,7 @@ const Home = props => {
                 <div className='box1'>
 
       <Swiper
-        slidesPerView={9}
+        slidesPerView={8}
         spaceBetween={30}
         slidesPerGroup={2}
         loop={true}
@@ -142,7 +145,7 @@ const Home = props => {
           </div>
           <div className='flex4'>
 
-          <h3>adil.dev1</h3>
+          <h3>code_with_adil</h3>
 
             <h5>adil marghadi</h5>
             </div>
@@ -194,8 +197,8 @@ const Home = props => {
 
     <div className='box4'>
           <div className='top'>
-                    <img src={profile1} alt='profile' />
-                    <p>joeel56</p> 
+                    <img src={img12} alt='profile' />
+                    <p>tiffinteck</p> 
                 <BiDotsHorizontalRounded className='dots'/>
                 </div>
                 <img src={img18} className='img2' alt='img' />
@@ -224,11 +227,86 @@ tiffintech</span> Monday vibes! Which mood are you today?!
 </h3>
 
         </div>
+
+
+
+
+
+
+        <div className='box4'>
+          <div className='top'>
+                    <img src={adilimg} alt='profile' />
+                    <p>adil.dev1</p> 
+                <BiDotsHorizontalRounded className='dots'/>
+                </div>
+                <img src={img21} className='img2' alt='img' />
+              <div className='bottom1'>
+                <div className='left1'>
+
+              <svg aria-label="Like" class="_8-yf5 " color="#262626" fill="#262626" height="24" role="img" viewBox="0 0 24 24" width="24"><path d="M16.792 3.904A4.989 4.989 0 0121.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 014.708-5.218 4.21 4.21 0 013.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 013.679-1.938m0-2a6.04 6.04 0 00-4.797 2.127 6.052 6.052 0 00-4.787-2.127A6.985 6.985 0 00.5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 003.518 3.018 2 2 0 002.174 0 45.263 45.263 0 003.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 00-6.708-7.218z"></path></svg>
+
+              <svg aria-label="Comment" class="_8-yf5 " color="#262626" fill="#262626" height="24" role="img" viewBox="0 0 24 24" width="24"><path d="M20.656 17.008a9.993 9.993 0 10-3.59 3.615L22 22z" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="2"></path></svg>
+
+              <svg aria-label="Share Post" class="_8-yf5 " color="#262626" fill="#262626" height="24" role="img" viewBox="0 0 24 24" width="24"><line fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="2" x1="22" x2="9.218" y1="3" y2="10.083"></line><polygon fill="none" points="11.698 20.334 22 3.001 2 3.001 9.218 10.084 11.698 20.334" stroke="currentColor" stroke-linejoin="round" stroke-width="2"></polygon></svg>
+              </div>
+
+            <div className='right1'>
+            <svg aria-label="Save" class="_8-yf5 " color="#262626" fill="#262626" height="24" role="img" viewBox="0 0 24 24" width="24"><polygon fill="none" points="20 21 12 13.44 4 21 4 3 20 3 20 21" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></polygon></svg>
+            </div>
+
+              </div>
+            <h2>6,352 likes</h2>
+            <h3><span className='big'>
+adil.dev1</span> The Only Way Is Up  💡
+
+ 
+</h3>
+           
+
+        </div>
+
+
+
+
+
+        <div className='box4'>
+          <div className='top'>
+                    <img src={img23} alt='profile' />
+                    <p>billieeilish
+</p> 
+                <BiDotsHorizontalRounded className='dots'/>
+                </div>
+                <img src={img24} className='img2' alt='img' />
+              <div className='bottom1'>
+                <div className='left1'>
+
+              <svg aria-label="Like" class="_8-yf5 " color="#262626" fill="#262626" height="24" role="img" viewBox="0 0 24 24" width="24"><path d="M16.792 3.904A4.989 4.989 0 0121.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 014.708-5.218 4.21 4.21 0 013.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 013.679-1.938m0-2a6.04 6.04 0 00-4.797 2.127 6.052 6.052 0 00-4.787-2.127A6.985 6.985 0 00.5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 003.518 3.018 2 2 0 002.174 0 45.263 45.263 0 003.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 00-6.708-7.218z"></path></svg>
+
+              <svg aria-label="Comment" class="_8-yf5 " color="#262626" fill="#262626" height="24" role="img" viewBox="0 0 24 24" width="24"><path d="M20.656 17.008a9.993 9.993 0 10-3.59 3.615L22 22z" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="2"></path></svg>
+
+              <svg aria-label="Share Post" class="_8-yf5 " color="#262626" fill="#262626" height="24" role="img" viewBox="0 0 24 24" width="24"><line fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="2" x1="22" x2="9.218" y1="3" y2="10.083"></line><polygon fill="none" points="11.698 20.334 22 3.001 2 3.001 9.218 10.084 11.698 20.334" stroke="currentColor" stroke-linejoin="round" stroke-width="2"></polygon></svg>
+              </div>
+
+            <div className='right1'>
+            <svg aria-label="Save" class="_8-yf5 " color="#262626" fill="#262626" height="24" role="img" viewBox="0 0 24 24" width="24"><polygon fill="none" points="20 21 12 13.44 4 21 4 3 20 3 20 21" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></polygon></svg>
+            </div>
+
+              </div>
+            <h2>6,352 likes</h2>
+            <h3><span className='big'>
+            billieeilish
+</span> TOUR’S BEEN A DREEEAAAMMMMMMM ✨🥰
+
+ 
+</h3>
+           
+
+        </div>
     </div>
             </div>
             </div>
          
-    )
+)
 }
 
 export default Home;
